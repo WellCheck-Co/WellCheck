a7637e676ff61977da3bd49a7e96ecb91f69f840
root <root@3c6514bb27ef>
2020-06-27 22:35:25 +0000

    new arch prep

--- landing/front/js/3D/xlayers-lite.esm.js
+++
@@ -1,6 +0,0 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-5be4e9d4.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["x-layers-page",[[2,"x-layers-page",{"data":[8],"page":[8],"wireframe":[4],"mode":[1]}]]],["x-layers-container",[[2,"x-layers-container",{"data":[8],"mode":[1],"zoom":[2],"wireframe":[4]}]]],["x-layers-upload",[[6,"x-layers-upload",null,[[0,"drop","onFileDrop"],[0,"dragover","dragOverHandler"],[0,"input","onFileChange"]]]]],["x-layers-layer",[[2,"x-layers-layer",{"data":[8],"layer":[8],"wireframe":[4],"depth":[2],"mode":[1],"borderWidth":[32],"offset3d":[32],"texts":[32],"images":[32],"layers":[32]}]]],["x-layers-canvas",[[2,"x-layers-canvas",{"data":[8],"mode":[1],"wireframe":[4],"zoom":[2],"currentPage":[32]}]]],["x-layers",[[2,"x-layers",{"src":[1],"mode":[1],"zoom":[2],"wireframe":[4],"data":[32]},[[0,"fileUploaded","onFileUploaded"]]]]]], options);
-});
--- landing/front/post.php
+++
@@ -1,6 +0,0 @@
-<?php
-$message = "| " . htmlspecialchars($_POST["email"]) . " | " . htmlspecialchars($_POST["message"]) . " | " . $_SERVER['REMOTE_ADDR'] . " | " . $_SERVER['HTTP_X_FORWARDED_FOR'] . " | " . date(DATE_RFC2822) . " | ";
-$fp = fopen('/result/contact.txt', 'a+');
-fwrite($fp, str_replace("\n","",$message) ."\n");
-fclose($fp);
-?>
