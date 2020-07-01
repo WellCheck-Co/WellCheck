40fcb688fe4c75d6c8ec52af27ab826c2b23e02b
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-07-01 20:57:17 +0200

    adding db update'

--- Wellcheck/Micro/sql/dumps/wellcheck.sql
+++ Wellcheck/Micro/sql/dumps/wellcheck.sql
@@ -1,19 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : mer. 01 juil. 2020 à 18:56
+-- Version du serveur :  5.7.29
+-- Version de PHP : 7.4.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données : `wellcheck`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orderdetails`
+--
 
 CREATE TABLE `orderdetails` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `json` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
@@ -22,6 +47,12 @@ CREATE TABLE `orders` (
   `date` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paymentstripe`
+--
+
 CREATE TABLE `paymentstripe` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -29,6 +60,12 @@ CREATE TABLE `paymentstripe` (
   `amount` float NOT NULL,
   `date` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `point`
+--
 
 CREATE TABLE `point` (
   `id` varchar(36) NOT NULL,
@@ -39,6 +76,12 @@ CREATE TABLE `point` (
   `date` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `point_shared`
+--
+
 CREATE TABLE `point_shared` (
   `id` varchar(36) NOT NULL,
   `id_user` int(11) NOT NULL,
@@ -47,13 +90,26 @@ CREATE TABLE `point_shared` (
   `surname` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `role` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(512) NOT NULL,
-  `date` varchar(64) NOT NULL
+  `date` varchar(64) NOT NULL,
+  `valid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `userdetails`
+--
 
 CREATE TABLE `userdetails` (
   `id` int(11) NOT NULL,
@@ -63,49 +119,103 @@ CREATE TABLE `userdetails` (
   `phone` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `userstripes`
+--
+
 CREATE TABLE `userstripes` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `stripe_id` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Index pour les tables déchargées
+--
 
+--
+-- Index pour la table `orderdetails`
+--
 ALTER TABLE `orderdetails`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Index pour la table `orders`
+--
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Index pour la table `paymentstripe`
+--
 ALTER TABLE `paymentstripe`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Index pour la table `point`
+--
+ALTER TABLE `point`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Index pour la table `userdetails`
+--
 ALTER TABLE `userdetails`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_id` (`user_id`);
 
+--
+-- Index pour la table `userstripes`
+--
 ALTER TABLE `userstripes`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_id` (`user_id`);
 
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
 
+--
+-- AUTO_INCREMENT pour la table `orderdetails`
+--
 ALTER TABLE `orderdetails`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT pour la table `orders`
+--
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT pour la table `paymentstripe`
+--
 ALTER TABLE `paymentstripe`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT pour la table `user`
+--
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT pour la table `userdetails`
+--
 ALTER TABLE `userdetails`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT pour la table `userstripes`
+--
 ALTER TABLE `userstripes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
