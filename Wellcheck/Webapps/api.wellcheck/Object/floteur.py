b5aeb4d62b49cb9d714c1de3d6b80a870b734172
benjamin.rousseliere <benjamin.rousseliere@gmail.com>
2020-09-26 19:43:34 +0200

    tests on sigfox

--- Wellcheck/Webapps/api.wellcheck/Object/floteur.py
+++ Wellcheck/Webapps/api.wellcheck/Object/floteur.py
@@ -261,18 +261,26 @@ class floteur:
         data = self.__infos_query(id_points, date_start, date_end, limit)
         return [True, {"detail": ret, "data": data}, None]
 
-    def note(self, ph, turb, orp, temp):
+    def note(self, ph, turbidity, redox, temp):
         """
             calculate a mark from all mesurement
         """
-        ph_point = 1.4 * 10 ** -32 - 29.3 * ph + 8.19 * ph ** 2 - 0.56 * ph ** 3
-        ph_point = 0 if float(ph_point) < float(0) else 5 if float(ph_point) > float(5) else ph_point
-        orp_point = -3.39 * 10 ** -32 - 0.0846 * turb + 6.73 * 10 ** -4 * turb ** 2 - 1.12 * 10 ** -6 * turb ** 3
-        orp_point = 0 if float(ph_point) < float(0) else 5 if float(ph_point) > float(5) else ph_point
-        temp_point = 5.83 + 0.235 * temp - 0.0241 * temp ** 2
-        temp_point = 0 if float(orp_point) < float(0) else 6 if float(orp_point) > float(6) else orp_point
-        turb_point = 0 if float(turb) < float(921) else 3
-        return float("{0:.1f}".format(ph_point + orp_point + temp_point + turb_point))
+        score = 0
+        if turbidity > 4.5:
+            score += 3
+        if ph > 6.5 and ph < 8.2:
+            phScore = 1.4 * pow(10, -32) -29.3 * ph + 8.19 * pow(ph, 2) - 0.56 * pow(ph, 3)
+            phScore = 0 if phScore < 0 else 5 if phScore > 5 else phScore
+            score += phScore
+        if redox > 220 and redox < 400:
+            redoxScore = -3.39 * pow(10, -32) -0.0846 * redox + 6.73 * pow(10, -4) * pow(redox, 2) - 1.12 * pow(10, -6) * pow(redox, 3)
+            redoxScore = 0 if redoxScore < 0 else 5 if redoxScore > 5 else redoxScore
+            score += redoxScore
+        if temp < 20:
+            tempScore = 5.83 + 0.235 * temp - 0.0241 * pow(temp, 2)
+            tempScore = 0 if tempScore < 0 else 6 if tempScore > 6 else tempScore
+            score += tempScore
+        return float("{0:.1f}".format(score))
 
     def inputrandom(self, id_point, id_sigfox, data, date):
         """
